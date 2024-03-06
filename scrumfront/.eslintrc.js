e9dddb614246cd12d7967614a94b50a96d267291
JitheshSoppa <jitheshsoppa2002@gmail.com>
2024-03-06 00:19:23 -0600

    dependencies install

---
+++ scrumfront/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": 2020,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "settings": {
+        "react": {
+          "version": "detect"
+        }
+      }
+}
