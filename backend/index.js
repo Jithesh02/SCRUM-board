20d9e374dc136b982bd0549a4abb45afdfa03cba
pranay rishith <52948338+pranayrishith16@users.noreply.github.com>
2024-03-06 20:25:53 -0600

    backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,28 @@
-const express = require('express');
+const express = require('express'); //backend
+const cors = require('cors'); // Allows access to permitted origins
+const bodyParser = require('body-parser'); // parse the request
+const connectDB = require('./db/connect'); // database connection
+
 const app = express();
+app.use(cors()); // allows for all origins
+app.use(bodyParser.json({ limit: '30mb', extended: true })) // parses the incoming request data into json into request.body
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public')); // accesses html, css, images from pubic folder
+
+const PORT = 8000
+
+url = 'mongodb+srv://testing:test123@cluster0.eboi2ye.mongodb.net/' // temp db link
+
+const start = async () => {
+    try {
+        await connectDB(url);
+        console.log('Connected to the database');
+        app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+    }
+
+    catch (error) {
+        console.log(error);
+    }   
+}
+
+start();
