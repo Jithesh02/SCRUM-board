b25c2974e4304f558fbf494a685b3223a37fbe4a
pranay rishith <52948338+pranayrishith16@users.noreply.github.com>
2024-03-09 16:35:23 -0600

    frontend

--- backend/scrumfront/src/containers/Sidebar.jsx
+++ backend/scrumfront/src/containers/Sidebar.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
+// eslint-disable-next-line no-unused-vars
+import SettingsIcon from '../icons/settings-svgrepo-com.svg'
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
-      Sidebar
+      <UpperContainer>
+
+      </UpperContainer>
+      <LowerContainer>
+        
+      </LowerContainer>
     </SidebarContainer>
   )
 }
@@ -15,4 +22,13 @@ const SidebarContainer = styled.div`
   width: 15vw;
   border-right: 1px solid #3a3a3a;
   height: 93vh;
+  padding-left: 2px;
+`
+
+const UpperContainer = styled.div`
+  border-bottom: 1px solid #3a3a3a;
+`
+
+const LowerContainer = styled.div`
+  
 `
