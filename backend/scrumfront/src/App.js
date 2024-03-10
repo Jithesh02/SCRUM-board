1d3a975abaf3a6ba5e68b02117bc6a6cf2f0a43e
pranay rishith <52948338+pranayrishith16@users.noreply.github.com>
2024-03-09 18:46:44 -0600

    Update App.js

--- backend/scrumfront/src/App.js
+++ backend/scrumfront/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components'
 import Nav from './containers/Nav';
 import Sidebar from './containers/Sidebar';
